da41b3f44656b097a8b17204981caa8ee03d494d
choiseonmun <seonmun.choi@gmail.com>
2025-05-20 15:26:18 +0900

    InventoryUI 레이아웃 구현

---
+++ Assets/Scripts/InventoryUI.cs
@@ -0,0 +1,22 @@
+using Gpm.Ui;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    public int Count = 10;
+    [SerializeField] private InfiniteScroll _infiniteScroll;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        for (int i = 0; i < Count; i++)
+        {
+            _infiniteScroll.InsertData(new InfiniteScrollData());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
